9ed6d7fcd84859c1a7828098224fa4ae8a128dc0
Fulcrum <sokatanocw@gmail.com>
2023-02-19 00:09:08 -0800

    notes backened complete

--- backend/controllers/noteController.js
+++ backend/controllers/noteController.js
@@ -27,6 +27,35 @@ const getNotes = asyncHandler(async (req, res) => {
   res.status(200).json(notes);
 });
 
+//@desc create ticket note
+//@Route POST /api/projects/:projectId/notes
+//@access Private
+const addNote = asyncHandler(async (req, res) => {
+  // Get user using the id in the JWT
+  const user = await User.findById(req.user.id);
+
+  if (!user) {
+    res.status(401);
+    throw new Error('User not found');
+  }
+
+  const project = await Project.findById(req.params.projectId);
+
+  if (project.user.toString() !== req.user.id) {
+    res.status(401);
+    throw new Error('User not authorized');
+  }
+
+  const note = await Note.create({
+    text: req.body.text,
+    isStaff: false,
+    project: req.params.projectId,
+    user: req.user.id,
+  });
+  res.status(200).json(note);
+});
+
 module.exports = {
   getNotes,
+  addNote,
 };
